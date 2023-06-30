02cd3c53b4f1fc635d940802fb5e529cafea6125
KainiChang <changkaini91@gmail.com>
2023-06-30 11:39:20 +0930

    cancel email timer

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 import { connectToDatabase } from "./utils/MySQL/db.js";
 import cors from "cors";
 import {corsOptions} from "./middleware/corslimits.js";
-import {startEmailTimer} from "./utils/timer/email/startEmailTimer.js";
+// import {startEmailTimer} from "./utils/timer/email/startEmailTimer.js";
 
 dotenv.config();
 
@@ -13,7 +13,6 @@ app.use(cors(corsOptions));
 const PORT = process.env.PORT||3000;
 
 connectToDatabase();
-console.log(`Server listening on port ${PORT}`);
 
 app.use(v1Router);
 
