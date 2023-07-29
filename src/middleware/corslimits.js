9658dd8658025511904d1af4c6b936c033f1149c
KainiChang <changkaini91@gmail.com>
2023-07-29 17:14:58 +0930

    add student end cors

--- src/middleware/corslimits.js
+++ src/middleware/corslimits.js
@@ -1,5 +1,5 @@
 const corsOptions = {
-    origin: 'https://victorious-pebble-0bf6fea10.3.azurestaticapps.net',
-    optionsSuccessStatus: 50 // some legacy browsers (IE11, various SmartTVs) choke on 204
+    origin: ['https://victorious-pebble-0bf6fea10.3.azurestaticapps.net','https://delightful-river-0ac974110.3.azurestaticapps.net'],
+    optionsSuccessStatus: 50 
   }
 export { corsOptions};
