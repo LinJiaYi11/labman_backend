2aab7afcfefc6dec6795a78d7790c29fff3789e2
KainiChang <changkaini91@gmail.com>
2023-07-29 17:36:52 +0930

    cors improve

--- src/middleware/corslimits.js
+++ src/middleware/corslimits.js
@@ -1,8 +1,8 @@
 const corsOptions = {
     origin: ['https://victorious-pebble-0bf6fea10.3.azurestaticapps.net','https://delightful-river-0ac974110.3.azurestaticapps.net'],
     optionsSuccessStatus: 50,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     preflightContinue: false,
-    optionsSuccessStatus: 204 
+    optionsSuccessStatus: 200 
   }
 export { corsOptions};
