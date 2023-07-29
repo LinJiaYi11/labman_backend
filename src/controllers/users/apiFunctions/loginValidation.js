382ece72db73684ed0fbdccc5eeddf6100dcaba7
KainiChang <changkaini91@gmail.com>
2023-07-29 19:21:41 +0930

    crypt revert

--- src/controllers/users/apiFunctions/loginValidation.js
+++ src/controllers/users/apiFunctions/loginValidation.js
@@ -1,5 +1,4 @@
 import pool from "../../../utils/MySQL/db.js";
-import bcrypt from "bcrypt";
 import errorMessages from "../../../utils/constants/errorMessages.js";
 import { checkUserExists } from "../helperFunctions/checkUserExists.js";
 export async function loginValidation(req, res) {
@@ -12,12 +11,9 @@ export async function loginValidation(req, res) {
 		const query = "SELECT password FROM students_user WHERE student_id = ?";
 		const [result] = await pool.query(query, [student_id]);
 		// console.log(result[0]);
-		const passwordMatches = await bcrypt.compare(password, result[0].password);
-
-        if (passwordMatches) {
-            return res.status(200).json({ message: "User logged in successfully" });
-        } 
-            return res.status(401).json({ message: "Incorrect username or password" });
+		if (result[0].password === password) {
+			return res.status(200).json({ message: "User logged in successfully" });
+		}
 	} catch (error) {
 		next(error);
 	}
