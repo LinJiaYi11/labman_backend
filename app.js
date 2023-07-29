82e4128bdba56180ca02b862a3428ea9cc9aea7c
KainiChang <changkaini91@gmail.com>
2023-07-29 18:12:13 +0930

    order reverse

--- app.js
+++ app.js
@@ -1,9 +1,6 @@
 import express from "express";
-import cors from "cors";
-import {corsOptions} from "./middleware/corslimits.js";
 
 const app = express();
-app.use(cors(corsOptions));
 
 app.use(express.json());
 
--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import app from "../app.js";
 import { v1Router } from "./routes/index.js";
 import dotenv from "dotenv";
 import { connectToDatabase } from "./utils/MySQL/db.js";
-
+import cors from "cors";
+import {corsOptions} from "./middleware/corslimits.js";
 // import {startEmailTimer} from "./utils/timer/email/startEmailTimer.js";
 
 dotenv.config();
+
+app.use(cors(corsOptions));
 
 const PORT = process.env.PORT||3000;
 
