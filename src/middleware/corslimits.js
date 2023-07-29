1a49fc2f9a596764940551aaad76b7930d0f23ca
KainiChang <changkaini91@gmail.com>
2023-07-29 17:51:02 +0930

    cores revert

--- src/middleware/corslimits.js
+++ src/middleware/corslimits.js
@@ -2,7 +2,7 @@ const corsOptions = {
     origin: ['https://victorious-pebble-0bf6fea10.3.azurestaticapps.net','https://delightful-river-0ac974110.3.azurestaticapps.net'],
     optionsSuccessStatus: 50,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    // preflightContinue: false,
+    preflightContinue: false,
     optionsSuccessStatus: 200 
   }
 export { corsOptions};
