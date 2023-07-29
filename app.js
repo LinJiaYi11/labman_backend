1976ddcdd02bf6ff29c4755c6721eb90f0e3da74
KainiChang <changkaini91@gmail.com>
2023-07-29 18:09:22 +0930

    cors order

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import cors from "cors";
+import {corsOptions} from "./middleware/corslimits.js";
 
 const app = express();
+app.use(cors(corsOptions));
 
 app.use(express.json());
 
